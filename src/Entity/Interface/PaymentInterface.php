62e87ca19dca46bf6290e98bbbd24c4095de3f11
Lucas-allard <allard.lucas.97@gmail.com>
2023-05-04 12:09:06 +0200

    ajout payment interface pour plus de modularité dans l'avenir

--- src/Entity/Interface/PaymentInterface.php
+++ src/Entity/Interface/PaymentInterface.php
@@ -2,7 +2,33 @@
 
 namespace App\Entity\Interface;
 
+use App\Entity\Order;
+
 interface PaymentInterface
 {
+    /**
+     * @return float
+     */
+    public function getAmount(): float;
+
+    /**
+     * @return string
+     */
+    public function getCurrency(): string;
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string;
+
+    /**
+     * @return string
+     */
+    public function getStatus(): string;
+
+    /**
+     * @retutests/Service/Stripe/StripConnexionTest.phprn Order|null
+     */
+    public function getOrder(): ?Order;
 
 }
