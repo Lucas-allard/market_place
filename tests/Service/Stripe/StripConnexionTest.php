4841ed76fe6902b1b4bcff71ede0c00333aa67bf
Lucas-allard <allard.lucas.97@gmail.com>
2023-05-04 12:08:09 +0200

    test stripe connexion

--- tests/Service/Stripe/StripConnexionTest.php
+++ tests/Service/Stripe/StripConnexionTest.php
@@ -2,7 +2,27 @@
 
 namespace App\Tests\Service\Stripe;
 
-class StripConnexionTest
+use App\Service\Stripe\StripeConnexion;
+use PHPUnit\Framework\TestCase;
+
+use Stripe\Stripe;
+
+class StripConnexionTest extends TestCase
 {
 
+    /**
+     * @group service
+     * @group stripe
+     * @group stripe-connexion
+     * @group stripe-connexion-init
+     */
+    public function testStripeConnexionGetStripe(): void
+    {
+        $apiKey = $_ENV['STRIPE_SECRET_KEY'];
+
+        StripeConnexion::init();
+
+        $this->assertSame($apiKey, Stripe::$apiKey);
+
+    }
 }
