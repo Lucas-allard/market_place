cbd56a0d37c8a9fec0c73aea803099bdd2af7061
Lucas-allard <allard.lucas.97@gmail.com>
2023-05-07 18:39:53 +0200

    ajout connexion

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/connexion', name: 'app_login')]
+    public function index(
+        AuthenticationUtils $authenticationUtils
+    ): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+
+    /**
+     * @throws Exception
+     */
+    #[Route('/deconnexion', name: 'app_logout')]
+    public function logout(): RedirectResponse
+    {
+        return $this->redirectToRoute('app_login');
+    }
+}
